4117fe93a5c9df7d0406fe3b2e4ca45243d233a6
Chinmay Chaudhari <chinmay@Chinmays-MacBook-Air.local>
2024-01-20 12:05:26 +0530

    info added

--- frontend/screens/profile/script.js
+++ frontend/screens/profile/script.js
@@ -4,6 +4,11 @@ console.log('ok')
 const email = localStorage.getItem('email');
 
 const comapnyDiv = document.getElementById('company-name');
+const ownerDiv = document.getElementById('owner');
+const coowner1Div = document.getElementById('coowner1');
+const coowner2Div = document.getElementById('coowner2');
+const addressDiv = document.getElementById('address');
+const phoneDiv = document.getElementById('phone');
 
 fetch(url + 'getuser' ,{
     method: "POST",
@@ -20,5 +25,10 @@ fetch(url + 'getuser' ,{
   })
   .then((json) => {
     comapnyDiv.innerText = json.user.company
+    ownerDiv.innerText = json.user.ownerDiv
+    coowner1Div.innerText = json.user.coowner1Div
+    coowner2Div.innerText = json.user.coowner2Div
+    addressDiv.innerText = json.user.address
+    phoneDiv.innerText = json.user.phone
     console.log(json)
   });
