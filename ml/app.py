352f4b3fce7c9d417f0782dc3079fbdb757ca3ea
Chinmay Chaudhari <chinmay@Chinmays-MacBook-Air.local>
2024-01-20 13:48:47 +0530

    ./ added

--- ml/app.py
+++ ml/app.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
-df = pd.read_csv('data.csv')
+df = pd.read_csv('./data.csv')
 df['profit'] = df['profit']
 
 X = df[['year']]
