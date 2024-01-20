a3df62195a6b3b09ce735f7e228e94828bd044dd
Chinmay Chaudhari <chinmay@Chinmays-MacBook-Air.local>
2024-01-20 18:05:43 +0530

    name bug fix

--- frontend/screens/contact/script.js
+++ frontend/screens/contact/script.js
@@ -1,3 +1,5 @@
+const url = "https://invictus-backend.vercel.app/";
+// const url = "http://localhost:5100/";
 document.addEventListener('DOMContentLoaded', function () {
     const button = document.querySelector('[data-collapse-toggle="navbar-default"]');
     const menu = document.getElementById('navbar-default');
@@ -6,3 +8,29 @@ document.addEventListener('DOMContentLoaded', function () {
         menu.classList.toggle('hidden');
     });
   });
+
+
+const companyElement = document.getElementById("company-name");
+
+const userEmail = localStorage.getItem("email");
+console.log(userEmail);
+fetch(url + "getuser", {
+  method: "POST",
+  body: JSON.stringify({
+    email: userEmail,
+  }),
+  headers: {
+    "Content-type": "application/json; charset=UTF-8",
+  },
+})
+  .then((resp) => resp.json())
+  .then((json) => {
+    companyElement.innerHTML = json.user.company
+    console.log(json);
+  });
+
+function signOut() {
+  localStorage.setItem("isLoggedIn", "false");
+  localStorage.setItem("email", "");
+  window.location.href = "../auth/index.html";
+}
